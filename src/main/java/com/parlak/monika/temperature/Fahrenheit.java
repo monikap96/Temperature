a834d15a2dca81892c6df41a30cc90e5508776fe
monikap96 <monikap96@interia.pl>
2019-04-19 13:04:05 +0200

    added TemperatureException

--- src/main/java/com/parlak/monika/temperature/Fahrenheit.java
+++ src/main/java/com/parlak/monika/temperature/Fahrenheit.java
@@ -1,15 +1,17 @@
 package com.parlak.monika.temperature;
 
 public class Fahrenheit extends Temperature {
-    public Fahrenheit(double temperature) {
+    public Fahrenheit(double temperature) throws TemperatureException {
         super(temperature);
+        if (toKelvin() < 0)
+            throw new TemperatureException();
     }
 
     public double toCelsius() {
-        return (temperature-32)*(5/9);
+        return (temperature - 32) * (5 / 9);
     }
 
     public double toKelvin() {
-        return (temperature+459.67)*(5/9);
+        return (temperature + 459.67) * (5 / 9);
     }
 }
---
+++ src/main/java/com/parlak/monika/temperature/TemperatureException.java
@@ -0,0 +1,5 @@
+package com.parlak.monika.temperature;
+
+public class TemperatureException extends Exception {
+
+}
